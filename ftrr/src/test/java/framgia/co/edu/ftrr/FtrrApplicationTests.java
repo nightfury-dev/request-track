79dd79c0f60cd58fdf2a0a4153ad4771284fb87d
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2019-02-12 11:53:21 +0700

    add structure init project

---
+++ ftrr/src/test/java/framgia/co/edu/ftrr/FtrrApplicationTests.java
@@ -0,0 +1,17 @@
+package framgia.co.edu.ftrr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FtrrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
