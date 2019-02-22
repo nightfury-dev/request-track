1facf484c1b65ffa214df6ce71483bb448b99271
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2019-02-22 08:58:07 +0700

    fix pull 12

--- ftrr/src/main/java/framgia/co/edu/ftrr/dto/request/RequestDTO.java
+++ ftrr/src/main/java/framgia/co/edu/ftrr/dto/request/RequestDTO.java
@@ -1,35 +1,35 @@
 package framgia.co.edu.ftrr.dto.request;
 
-import java.util.Date;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import framgia.co.edu.ftrr.common.RequestStatus;
-import framgia.co.edu.ftrr.entity.TraineeForRequest;
+import framgia.co.edu.ftrr.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 
-public class RequestDTO {
+public class RequestDTO implements java.io.Serializable {
 
-	private Integer id;
-	private UserDTO createdBy;
-	private UserDTO updatedBy;
-	private String division;
-	private Integer quantity;
-	private Date deadline;
-	private String language;
-	private RequestStatus status;
-	private String createdAt;
-	private String updatedAt;
-	@JsonIgnore
-	private List<TraineeForRequest> traineeForRequests;
+    private Integer id;
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "encryptedPassword", "resetPasswordToken",
+            "resetPasswordSentAt", "rememberCreatedAt", "confirmationToken", "confirmationAt", "confirmationSentAt"})
+    private User createdBy;
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "encryptedPassword", "resetPasswordToken",
+            "resetPasswordSentAt", "rememberCreatedAt", "confirmationToken", "confirmationAt", "confirmationSentAt"})
+    private User updatedBy;
+    private String division;
+    private Integer quantity;
+    private Date deadline;
+    private String language;
+    private RequestStatus status;
+    private String createdAt;
+    private String updatedAt;
 
 }
--- ftrr/src/main/java/framgia/co/edu/ftrr/repository/UserRepository.java
+++ ftrr/src/main/java/framgia/co/edu/ftrr/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package framgia.co.edu.ftrr.repository;
 
+import framgia.co.edu.ftrr.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import framgia.co.edu.ftrr.entity.User;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	User findByEmail(String email);
-	User findOneByEmail (String email);
 }
