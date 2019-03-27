73b58a53251ebd4cd07ada209d009f23cb753cc0
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2019-03-27 15:24:37 +0700

    swagger

--- ftrr/src/main/java/framgia/co/edu/ftrr/FtrrApplication.java
+++ ftrr/src/main/java/framgia/co/edu/ftrr/FtrrApplication.java
@@ -2,12 +2,28 @@ package framgia.co.edu.ftrr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class FtrrApplication{
+@EnableSwagger2
+public class FtrrApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FtrrApplication.class, args);
+    }
+
+    @Bean
+    public Docket studentAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
 
--- ftrr/src/main/java/framgia/co/edu/ftrr/config/WebSecurityConfig.java
+++ ftrr/src/main/java/framgia/co/edu/ftrr/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -68,4 +69,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return bCryptPasswordEncoder;
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs/**");
+        web.ignoring().antMatchers("/swagger.json");
+        web.ignoring().antMatchers("/swagger-ui.html");
+        web.ignoring().antMatchers("/swagger-resources/**");
+        web.ignoring().antMatchers("/webjars/**");
+    }
 }
